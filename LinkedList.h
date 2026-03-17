2ffe9113e7e2e6c45ef5bb9b285dd48582520879
htlinh1204-wq <htlinh1204@gmail.com>
2026-03-17 11:12:10 +0700

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -11,14 +11,15 @@ struct Node
 	Node *next;
 };
 
+
 //Ham khoi tao ban dau
 inline void init(Node *&head) {
 	head = nullptr;
 }
 
-// Ham tao node moi
 
-inline Node* createNode(int x) {
+// Ham tao node moi
+inline Node* createNode(int x){
 	Node* p = new  Node;
 	if (p == nullptr)
 		return nullptr;
@@ -26,6 +27,15 @@ inline Node* createNode(int x) {
 	p->next = nullptr;
 	return p;
 }
+
+//Ham them dau
+inline void Insert_First(int x) {
+	Node* p = createNode(x);
+	if (true)
+	{
+
+	}
+}
+
 #endif // !Linked_List
 #pragma once
-
