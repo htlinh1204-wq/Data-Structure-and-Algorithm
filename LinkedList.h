1666d1e1ca38660246ac7abec31217bc9f8db72b
htlinh1204-wq <htlinh1204@gmail.com>
2026-03-17 11:07:39 +0700

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -1,1 +1,31 @@
+#ifndef Linked_List
+#define Linked_List
+
+#include<iostream>
+using namespace std;
+
+//Dinh nghia kieu du lieu Node
+struct Node
+{
+	int data;
+	Node *next;
+};
+
+//Ham khoi tao ban dau
+inline void init(Node *&head) {
+	head = nullptr;
+}
+
+// Ham tao node moi
+
+inline Node* createNode(int x) {
+	Node* p = new  Node;
+	if (p == nullptr)
+		return nullptr;
+	p->data = x;
+	p->next = nullptr;
+	return p;
+}
+#endif // !Linked_List
+#pragma once
 
